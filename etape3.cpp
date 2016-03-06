2328c67f9950f951f57b7e1f0553765a3a62c9c4
Fieldrain <bonfilsadrien@gmail.com>
2016-03-06 16:35:09 +0100

    Fichier thomas B

--- etape3.cpp
+++ etape3.cpp
@@ -6,60 +6,92 @@
 
 using namespace std;
 
+/*
+	Définition de la structure de définition des agents
+*/
+enum d_classe_attribut {INT, FLOAT, STRING, CHAR, BOOL};
 
-enum classe_attribut {INT, FLOAT,STRING,CHAR,BOOL};
-
-struct type_attribut {
-	classe_attribut c_attribut;
+struct d_type_attribut {
+	d_classe_attribut c_attribut;
 	bool interval;
 	float min;
 	float max;
 };
 
-struct attribut {
+struct d_attribut {
 	string nom_attribut;
-	type_attribut type;
+	d_type_attribut type;
 };
 
-struct agent {
+struct d_agent {
 	string nom_agent;
 	int nb_attribut;
 	attribut attr[100];
 };
 
+struct d_liste_agent {
+	agent  l_agent[1000];
+	int nb_agent=0;
+};
 
-agent liste_agent[1000];
-int nbAgent=0;
+d_liste_agent liste_type_agent;
+
+/*
+	Définition de la structure de stockage des agents
+*/
+
+struct a_attribut {
+	string nom_attribut;
+	union { int the_int, float the_float, string the_string, char the_char, bool the_bool};
+	float min;
+	float max;
+};
+
+struct a_agent {
+	string type_agent;
+	string nom_agent;
+	a_attribut attribut[100];
+};
+
+struct a_liste_agent {
+	a_agent liste[1000];
+};
+
+a_liste_agent liste_agent;
+/*
+	Fonction de gestion de la définition des agents
+*/
+
 bool insererTypeAgent(string nom);
 bool verifierNomAgent(string nomAgent);
-bool insererAttribut(string nom, classe_attribut type);
-bool verifierNomAttribut(string nomAttribut);
+bool insererattribut(string nom, d_classe_attribut type);
+bool verifierNomattribut(string nomattribut);
 
 
 bool insererTypeAgent(string nom) {
 	if(verifierNomAgent(nom)) {
-		liste_agent[nbAgent].nom_agent = nom;
-		liste_agent[nbAgent].nb_attribut =0;
-		nbAgent++;
+		liste_type_agent.l_agent[nb_agent].nom_agent = nom;
+		liste_type_agent.l_agent[nb_agent].nb_attribut =0;
+		liste_type_agent.nb_agent++;
 		return true;
 	}
 	return false;
 }
 
 bool verifierNomAgent(string nomAgent){
-	for (int i =0; i < nbAgent; i++) {
-		if (nomAgent == liste_agent[i].nom_agent) {
+	for (int i =0; i < liste_type_agent.nb_agent; i++) {
+		if (nomAgent ==  liste_type_agent.l_agent[i].nom_agent) {
 			return false;
 		}
 	}
 	return true;
 }
 
-bool insererAttribut(string nom, classe_attribut type){
-       agent a = liste_agent[nbAgent];
-       if(verifierNomAttribut(nom)){
+bool insererattribut(string nom, d_classe_attribut type){
+       d_agent a =  liste_type_agent.l_agent[liste_type_agent.nb_agent];
+       if(verifierNomattribut(nom)){
              a.nb_attribut++;
-             attribut at;
+             d_attribut at;
              at.nom_attribut = nom;
              at.type.c_attribut = type;
              a.attr[a.nb_attribut]=at;
@@ -68,9 +100,9 @@ bool insererAttribut(string nom, classe_attribut type){
        return false;
 }
 
-bool verifierNomAttribut(string nomAttribut){
-       for(int i=0;i<liste_agent[nbAgent].nb_attribut;i++){
-             if(nomAttribut==liste_agent[nbAgent].attr[i].nom_attribut){
+bool verifierNomattribut(string nomattribut){
+       for(int i=0;i<liste_type_agent.l_agent[liste_type_agent.nb_agent].nb_attribut;i++){
+             if(nomattribut==liste_type_agent.l_agent[liste_type_agent.nb_agent].attr[i].nom_attribut){
                    return false;
              }
        }
